d1c1507381c48d0dd3991fd0850c598809a4e8bd
Brian Gathuita <bgathuita@gmail.com>
2026-06-24 12:03:31 +0300

    Allow blogger.googleusercontent.com images for real WC2026 jersey kit photos

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,8 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' },
       { protocol: 'https', hostname: 'media.graphassets.com' },
+      { protocol: 'https', hostname: 'blogger.googleusercontent.com' },
+      { protocol: 'https', hostname: '*.googleusercontent.com' },
     ],
   },
 };
